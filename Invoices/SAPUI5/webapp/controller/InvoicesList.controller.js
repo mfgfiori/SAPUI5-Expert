5ae4c25bcf532ff39cf04f3fe8bf58d2cfde081d
mfgfiori <mfdezgarcia@gmail.com>
2021-03-04 19:45:20 +0000

    Laboratorio 5

--- Invoices/SAPUI5/webapp/controller/InvoicesList.controller.js
+++ Invoices/SAPUI5/webapp/controller/InvoicesList.controller.js
@@ -2,14 +2,18 @@
 sap.ui.define([
     'sap/ui/core/mvc/Controller',
     'sap/ui/model/json/JSONModel',
-    '../model/InvoicesFormatter'
+    '../model/InvoicesFormatter',
+    'sap/ui/model/Filter',
+    'sap/ui/model/FilterOperator'
 ], 
 /**
  * 
  * @param { typeof sap.ui.core.mvc.Controller } Controller 
  * @param { typeof sap.ui.model.json.JSONModel } JSONModel
+ * @param { typeof sap.ui.model.Filter } Filter
+ * @param { typeof sap.ui.model.FilterOperator } FilterOperator
  */
-function(Controller, JSONModel, InvoicesFormatter) {
+function(Controller, JSONModel, InvoicesFormatter, Filter, FilterOperator) {
 
     return Controller.extend("logaligroup.SAPUI5.controller.InvoicesList", {
 
@@ -21,8 +25,21 @@ function(Controller, JSONModel, InvoicesFormatter) {
                 eur: "EUR"
             });
             this.getView().setModel(oViewModel, "currency");
-        }
+        },
+                
+        onFilterInvoices : function(oEvent){
+            const aFilter = [];
+            
+            const sQuery = oEvent.getParameter("query");
 
+            if (sQuery){
+                aFilter.push( new Filter("ProductName", FilterOperator.Contains, sQuery))
+            };
+            const oList = this.getView().byId("invoiceList");
+            const oBinding = oList.getBinding("items");
+
+            oBinding.filter(aFilter);
+        }  
     });
 
 });

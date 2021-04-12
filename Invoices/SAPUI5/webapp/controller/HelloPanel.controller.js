23416dd3a20dcc65cab453fca38573c0376de87b
mfgfiori <mfdezgarcia@gmail.com>
2021-04-12 12:58:37 +0000

    15_Tools_Depuracion

--- Invoices/SAPUI5/webapp/controller/HelloPanel.controller.js
+++ Invoices/SAPUI5/webapp/controller/HelloPanel.controller.js
@@ -3,15 +3,24 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/m/MessageToast",
+    "sap/base/Log"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      * @param {typeof sap.m.MessageToast} MessageToast
-    
+     * @param {typeof sap.base.Log} Log
      */
-    function (Controller, MessageToast) {
+    function (Controller, MessageToast, Log) {
         "use strict";
         return Controller.extend("logaligroup.SAPUI5.controller.HelloPanel", {
+            onBeforeRendering: function (){
+                 window.message = 'Log message - onBeforeRendering';
+                 Log.info(window.message);
+                 Log.error(window.message);
+            },
+            onAfterRendering: function (){
+                 debugger;
+            },
 
             onInit: function () {
 
@@ -30,7 +39,6 @@ sap.ui.define([
             onOpenDialog: function () {
                 this.getOwnerComponent().openHelloDialog();
 
-            },
-
+            }
         });
     });
